463d52772259ecb0715f0436385d23c3541a0690
NicholasYang21 <1011268631@qq.com>
2022-10-06 21:47:30 +0800

    .

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,15 @@ use tui::{
     backend::CrosstermBackend,
     backend::Backend,
     Frame,
-    widgets::{Block, Borders},
+    widgets::{Block, Borders, Clear},
     layout::{Layout, Constraint, Direction},
+    style::Style,
+    style::Color::*,
     Terminal
 };
 use crossterm::{event::read, event::{EnableMouseCapture}, execute, terminal::{enable_raw_mode, EnterAlternateScreen}, event};
 use crossterm::event::KeyCode::Esc;
+use tui::text::Span;
 
 fn ui<B: Backend>(f: &mut Frame<B>) {
     let chunks = Layout::default()
@@ -23,14 +26,24 @@ fn ui<B: Backend>(f: &mut Frame<B>) {
         .split(f.size());
 
     let block = Block::default()
-        .title("Block")
-        .borders(Borders::ALL);
+        .title(Span::styled("Styled Title",
+                            Style::default().fg(Rgb(0xff, 0xc6, 0x6d))))
+        .borders(Borders::ALL)
+        .border_style(Style::default().fg(Red));
+
     f.render_widget(block, chunks[0]);
 
     let block = Block::default()
-        .title("Block 2")
-        .borders(Borders::ALL);
+        .title(Span::styled("Styled Title 2",
+                            Style::default().fg(Green)))
+        .borders(Borders::ALL)
+        .border_style(Style::default().fg(White).bg(Red));
+
     f.render_widget(block, chunks[1]);
+}
+
+fn clear_screen<B: Backend>(f: &mut Frame<B>) {
+    f.render_widget(Clear, f.size());
 }
 
 fn main() -> Result<(), io::Error> {
@@ -41,10 +54,10 @@ fn main() -> Result<(), io::Error> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
+    terminal.draw(|f| { clear_screen(f); })?;
+
     loop {
-        terminal.draw(|f| {
-            ui(f)
-        })?;
+        terminal.draw(|f| { ui(f) })?;
 
         if let event::Event::Key(event) = read()? {
             if event.code == Esc {
