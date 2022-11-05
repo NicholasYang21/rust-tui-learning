7ee8ee7280ec5021cb5ab947cea984b0545b4a0b
nicho <1011268631@qq.com>
2022-11-05 12:17:32 +0800

    .

--- target/debug/deps/rust_tui_learning-33e91b42d9dd17fd.d
+++ target/debug/deps/rust_tui_learning-33e91b42d9dd17fd.d
@@ -1,10 +1,8 @@
-/home/nicho/rust-tui-learning/target/debug/deps/rust_tui_learning-33e91b42d9dd17fd.rmeta: src/main.rs src/tui_rs/mod.rs src/tui_rs/helloworld.rs src/cursive_tui/mod.rs src/cursive_tui/helloworld.rs
+/home/nicho/rust-tui-learning/target/debug/deps/rust_tui_learning-33e91b42d9dd17fd.rmeta: src/main.rs src/cursive_tui/mod.rs src/cursive_tui/helloworld.rs
 
-/home/nicho/rust-tui-learning/target/debug/deps/rust_tui_learning-33e91b42d9dd17fd.d: src/main.rs src/tui_rs/mod.rs src/tui_rs/helloworld.rs src/cursive_tui/mod.rs src/cursive_tui/helloworld.rs
+/home/nicho/rust-tui-learning/target/debug/deps/rust_tui_learning-33e91b42d9dd17fd.d: src/main.rs src/cursive_tui/mod.rs src/cursive_tui/helloworld.rs
 
 src/main.rs:
-src/tui_rs/mod.rs:
-src/tui_rs/helloworld.rs:
 src/cursive_tui/mod.rs:
 src/cursive_tui/helloworld.rs:
 
